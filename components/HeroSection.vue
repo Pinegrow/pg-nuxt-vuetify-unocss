6d38c1d8507b804ae8d5ee41f3101de71752ca54
Akayy <techakayy@gmail.com>
2024-06-06 17:03:12 +1000

    Initial commit

---
+++ components/HeroSection.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+  import { heroImageUrl } from '@/utils/hero'
+
+  const { optimizeImage } = useOptimizeImage()
+  const heroImageOptimized = {
+    alt: `hero`,
+    cover: true,
+    ...optimizeImage(
+      heroImageUrl,
+      /* options */
+      {
+        /* If using local images instead of unsplash url, enable netlify provider */
+        // provider:
+        //     process.env.NODE_ENV === 'production'
+        //       ? 'netlify'
+        //       : null /* defaults to ipx or ipxStatic */,
+        placeholder: false, // placeholder image before the actual image is fully loaded.
+      },
+      true /* return bgStyles */,
+    ),
+  }
+
+  const heroImage = heroImageOptimized.src
+  const bgStyles = heroImageOptimized.bgStyles
+</script>
+<template>
+  <section>
+    <v-img
+      :src="heroImageOptimized.src"
+      :srcset="heroImageOptimized.srcset"
+      :sizes="heroImageOptimized.sizes"
+      :max-height="800"
+      cover
+    >
+      <!-- <v-img :src="heroImageUrl" :max-height="800" cover> -->
+      <div class="container mx-auto">
+        <div
+          class="pb-36 pt-2 px-6 relative rounded-3xl md:pb-48 lg:pb-72 lg:px-12"
+        >
+          <div
+            class="-mx-4 flex flex-wrap items-center mt-32 space-y-6 lg:space-y-0"
+          >
+            <div class="px-4 w-full md:w-8/12 xl:w-6/12">
+              <h2
+                class="capitalize font-serif leading-tight mb-2 md:w-96 text-primary w-60 xl:w-full"
+              >
+                Hot Deals on Fresh Styles!
+              </h2>
+              <v-btn
+                text="Shop Now"
+                variant="elevated"
+                size="large"
+                class="mt-4"
+                to="/women"
+              ></v-btn>
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-img>
+  </section>
+</template>
+<style scoped></style>
---
+++ themes/pg-tailwindcss/tokens.mjs
@@ -0,0 +1,146 @@
+/* Pinegrow generated Design Panel Begin */
+/* eslint-disable */
+/* prettier-disable */
+/* @ts-nocheck */
+
+const pg_colors = {
+  primary: {
+    50: '#f5ecec',
+    100: '#efcfc9',
+    200: '#e7b2a8',
+    300: '#dd9587',
+    400: '#d17968',
+    500: '#c35b4a',
+    600: '#b43c2d',
+    700: '#a53729',
+    800: '#963226',
+    900: '#872d22',
+    950: '#79281e',
+    DEFAULT: '#6a231b',
+  },
+  secondary: {
+    50: '#f0ebeb',
+    100: '#e3c7c3',
+    200: '#d3a49b',
+    300: '#c18276',
+    400: '#ae6052',
+    500: '#983d31',
+    600: '#811411',
+    700: '#761210',
+    800: '#6b110f',
+    900: '#610f0e',
+    950: '#560e0c',
+    DEFAULT: '#4c0c0a',
+  },
+  tertiary: {
+    50: '#f8f0ee',
+    100: '#f3ded7',
+    200: '#edcbbf',
+    300: '#e6b8a9',
+    400: '#dea692',
+    500: '#d6947c',
+    600: '#cd8267',
+    700: '#bc775e',
+    800: '#ab6c56',
+    900: '#9a624d',
+    950: '#895745',
+    DEFAULT: '#794d3d',
+  },
+  success: {
+    50: '#ecf0ec',
+    100: '#cdddcb',
+    200: '#aec9ab',
+    300: '#90b68c',
+    400: '#71a36d',
+    500: '#519050',
+    600: '#2e7d32',
+    700: '#2a722e',
+    800: '#26682a',
+    900: '#235e26',
+    950: '#1f5422',
+    DEFAULT: '#519050',
+  },
+  warning: {
+    50: '#fcefeb',
+    100: '#ffd9c7',
+    200: '#ffc4a4',
+    300: '#ffae7f',
+    400: '#fb985c',
+    500: '#f58238',
+    600: '#ed6c02',
+    700: '#d96302',
+    800: '#c55a02',
+    900: '#b25102',
+    950: '#9f4801',
+    DEFAULT: '#f58238',
+  },
+  error: {
+    50: '#f9ecec',
+    100: '#f9cfca',
+    200: '#f6b2a9',
+    300: '#f19488',
+    400: '#e97669',
+    500: '#df564c',
+    600: '#d32f2f',
+    700: '#c12b2b',
+    800: '#b02727',
+    900: '#9e2323',
+    950: '#8d2020',
+    DEFAULT: '#df564c',
+  },
+  info: {
+    50: '#ebf1f9',
+    100: '#d0dff2',
+    200: '#b4ccec',
+    300: '#96bbe5',
+    400: '#76a9df',
+    500: '#5098d8',
+    600: '#0288d1',
+    700: '#027cbf',
+    800: '#0271ae',
+    900: '#02669d',
+    950: '#015b8c',
+    DEFAULT: '#5098d8',
+  },
+  neutral: {
+    50: '#ebebeb',
+    100: '#c4c4c4',
+    200: '#9f9f9f',
+    300: '#7b7b7b',
+    400: '#585858',
+    500: '#383838',
+    600: '#1b1b1b',
+    700: '#191919',
+    800: '#171717',
+    900: '#151515',
+    950: '#131313',
+    DEFAULT: '#101010',
+  },
+}
+
+const pg_fonts = {
+  sans: ["'Inter', sans-serif"],
+  serif: ["'Kalam', sans-serif"],
+}
+
+const pg_font_urls = [
+  'https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900|Kalam:300,400,700&display=swap',
+]
+
+const pg_backgrounds = {
+  'design-image':
+    "url('https://images.unsplash.com/photo-1513373319109-eb154073eb0b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDI0MXx8ZmFzaGlvbiUyMHNob3BwaW5nJTIwYmFnfGVufDB8fHx8MTY5Njg3MTQwNXww&ixlib=rb-4.0.3&q=80&w=1080')",
+  'design-image-large':
+    "url('https://images.unsplash.com/photo-1513373319109-eb154073eb0b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDI0MXx8ZmFzaGlvbiUyMHNob3BwaW5nJTIwYmFnfGVufDB8fHx8MTY5Njg3MTQwNXww&ixlib=rb-4.0.3&q=80&w=2000')",
+}
+
+const pg_background_urls = {
+  'design-image':
+    'https://images.unsplash.com/photo-1513373319109-eb154073eb0b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDI0MXx8ZmFzaGlvbiUyMHNob3BwaW5nJTIwYmFnfGVufDB8fHx8MTY5Njg3MTQwNXww&ixlib=rb-4.0.3&q=80&w=1080',
+  'design-image-large':
+    'https://images.unsplash.com/photo-1513373319109-eb154073eb0b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDI0MXx8ZmFzaGlvbiUyMHNob3BwaW5nJTIwYmFnfGVufDB8fHx8MTY5Njg3MTQwNXww&ixlib=rb-4.0.3&q=80&w=2000',
+}
+
+export { pg_colors, pg_fonts, pg_font_urls, pg_backgrounds, pg_background_urls }
+
+/* Pinegrow generated Design Panel End */
